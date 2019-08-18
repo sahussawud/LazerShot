289939266262963a9e7d73c99acb6461291753f1
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2019-08-19 01:09:30 +0700

    Add files via upload

--- Front_end By Pond/index.php
+++ Front_end By Pond/index.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <?php
-    include("mqtt_test.php")
+include("mqtt_test.php")
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <!-- background -->
-    <div class="stars"></div>
-    <div class="twinkling"></div>
+<!-- background -->
+<div class="stars"></div>
+<div class="twinkling"></div>
 <head>
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>LAZERSHOT</title>
+
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="./style.css">
@@ -32,47 +33,73 @@
             <h1 style="color: aqua;">DASHBOARD</h1>
         </div>
     </div>
-    <div class="container-fluid">
-
-        <div class="media col-sm-4">
-            <div class="media-left">
-                <img src="https://cdn.shopify.com/s/files/1/2265/4045/products/Productshot.jpg?v=1545336364" class="media-object" style="width:100px">
-            </div>
-                <div class="media-body">
-                    <h4 class="media-heading" style="color: azure">NAME : IP 198.168.0.1</h4>
-                    <img src="https://thumbs.gfycat.com/WideShowyJohndory.webp" style="width: 180px">
-                </div>
-            </div>
-        </div>
 
     <!-- HP BAR_V.1 -->
 
-    <div class="row">
-            <div class="col-sm-4"><canvas id="canvas3" width="400" height="400"></canvas></div>
-            <div class="col-sm-1"><input id="get_rpm" type="number"/></div>
-    </div>
+    <!-- <div class="row">
+        <div class="col-sm-4"><canvas id="canvas3" width="400" height="400"></canvas></div>
+        <div class="col-sm-1"><input id="get_rpm" type="number"/></div>
+    </div> -->
+
+    <!-- HP BAR_V.2 -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600,800,900" rel="stylesheet" type="text/css">
+    <div id="container"></div>
 
     <div id="timer">00:00</div>
     <br><br>
-    <div style="text-align: center;">
-        <div class="col-md-1 col-sm-1 col-xs-3"> <a onclick="start('S')" class="btn btn-sm animated-button thar-two">Start</a> </div>
-        <!-- <button class="button Start" onclick="start('S')">Start</button> -->
-        <button class="button Start" onclick="start('P')">Pause</button>
-        <button class="button Start" onclick="start('R')">Reset</button>
-
+    <div id="button_time">
+        <div class="col-md-1"> <a onclick="start('S')" class="btn btn-sm animated-button thar-two">Start</a> </div>
+        <div class="col-md-1"> <a onclick="start('P')" class="btn btn-sm animated-button thar-two">Pause</a> </div>
+        <div class="col-md-1"> <a onclick="start('R'), send('R')" class="btn btn-sm animated-button thar-two">Reset</a> </div>
     </div>
-    <!-- ศส่วนของ MQTT ของอ๋อง -->
-
+    <!-- ส่วนของ MQTT ของอ๋อง -->
+    <br><br>
+    <div id="button_time">
         <button class="button" onclick="send('1')">+</button>
         <button class="button" onclick="send('-1')">-</button>
-        <button class="button" onclick="random()">Random</button>
-        <button class="button" onclick="send('R')">Reset</button>
+    </div>
+    <br><br>
     <!-- การส่งค่าขึ้น MQTT -->
-    <input type="text" id="score" placeholder="Score" required></p>
-    <button class="btn-hover color-2" onclick="send('S')">send</button>
-    <p id="sub_mqtt">X</p>
-    <p id="number">1</p>
+    <div id="button_time">
+        <input type="text" id="score" placeholder="Score" required></p>
+        <button class="button" onclick="send('S')">send</button>
+    </div>
+    <p id="sub_mqtt">Score: 0</p>
+    <p id="player_hp">HP: 100%</p>
+    <p id="number">0-0</p>
+        <div class="row">
+            <div class="col-md-4">
+                <a onclick="test_score('1')" class="btn btn-sm animated-button thar-two">1</a>
+            </div>
+            <div class="col-md-4">
+                <a onclick="test_score('2')" class="btn btn-sm animated-button thar-two">2</a>
+            </div>
+            <div class="col-md-4">
+                <a onclick="test_score('3')" class="btn btn-sm animated-button thar-two">3</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <a onclick="test_score('4')" class="btn btn-sm animated-button thar-two">4</a>
+            </div>
+            <div class="col-md-4">
+                <a onclick="test_score('5')" class="btn btn-sm animated-button thar-two">5</a>
+            </div>
+            <div class="col-md-4">
+                <a onclick="test_score('6')" class="btn btn-sm animated-button thar-two">6</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <a onclick="test_score('7')" class="btn btn-sm animated-button thar-two">7</a>
+            </div>
+            <div class="col-md-4">
+                <a onclick="test_score('8')" class="btn btn-sm animated-button thar-two">8</a>
+            </div>
+            <div class="col-md-4">
+                <a onclick="test_score('9')" class="btn btn-sm animated-button thar-two">9</a>
+            </div>
+        </div>
 </body>
 
-</body>
 </html>
--- Front_end By Pond/mqtt_test.php
+++ Front_end By Pond/mqtt_test.php
@@ -1,101 +1,45 @@
-
 <script src="paho.javascript-1.0.3/paho-mqtt.js"></script>
 <script>
-    client = new Paho.MQTT.Client("broker.mqttdashboard.com", Number(8000), "ongsuwannoo");
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
+  var clientid = Math.floor(Math.random() * (5000 - 1000)) + 1000;
+  client = new Paho.MQTT.Client("broker.mqttdashboard.com", Number(8000), String(clientid));
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
 
-    client.connect({onSuccess:onConnect});
-    var score = 0;
+  client.connect({onSuccess:onConnect});
+  var score = 0;
+  var topic_sup = "test_input";
+  var topic_pub = "test_output";
+  function onConnect() {
+    console.log("onConnect");
+    client.subscribe(topic_sup);
+  }
 
-    function onConnect() {
-      console.log("onConnect");
-      client.subscribe("test_input");
+  function send(msg){
+    if (msg == 'R'){
+      score = 0;
+      document.getElementById("sub_mqtt").innerHTML = "Score: "+score;
     }
-
-    function send(msg){
-      if (msg == 'R'){
-        score = 0;
-        document.getElementById("sub_mqtt").innerHTML = score;
-      }
-      else if (msg == 'S'){
-        message = new Paho.MQTT.Message(document.getElementById("score").value);
-        message.destinationName = "rew";
-        client.send(message);
-      }
-      else{
-        message = new Paho.MQTT.Message(msg);
-        message.destinationName = "rew";
-        client.send(message);
-      }
+    else if (msg == 'S'){
+      message = new Paho.MQTT.Message(document.getElementById("score").value);
+      message.destinationName = topic_pub;
+      client.send(message);
     }
-
-    function onConnectionLost(responseObject) {
-      if (responseObject.errorCode !== 0) {
-        console.log("onConnectionLost:"+responseObject.errorMessage);
-      }
+    else{
+      message = new Paho.MQTT.Message(msg);
+      message.destinationName = topic_pub;
+      client.send(message);
     }
+  }
 
-    function onMessageArrived(message) {
-      console.log("onMessageArrived:"+message.payloadString);
-      score += Number(message.payloadString);
-      document.getElementById("sub_mqtt").innerHTML = score;
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
     }
+  }
 
-    function random(){
-      var min = 1;
-      var max = 16;
-      number = Math.floor(Math.random() * (+max - +min)) + +min;
-      document.getElementById("number").innerHTML = number;
-    }
-
-<script src="paho.javascript-1.0.3/paho-mqtt.js"></script>
-<script>
-    client = new Paho.MQTT.Client("broker.mqttdashboard.com", Number(8000), "ongsuwannoo");
-    client.onConnectionLost = onConnectionLost;
-    client.onMessageArrived = onMessageArrived;
-
-    client.connect({onSuccess:onConnect});
-    var score = 0;
-
-    function onConnect() {
-      console.log("onConnect");
-      client.subscribe("test_input");
-    }
-
-    function send(msg){
-      if (msg == 'R'){
-        score = 0;
-        document.getElementById("sub_mqtt").innerHTML = score;
-      }
-      else if (msg == 'S'){
-        message = new Paho.MQTT.Message(document.getElementById("score").value);
-        message.destinationName = "rew";
-        client.send(message);
-      }
-      else{
-        message = new Paho.MQTT.Message(msg);
-        message.destinationName = "rew";
-        client.send(message);
-      }
-    }
-
-    function onConnectionLost(responseObject) {
-      if (responseObject.errorCode !== 0) {
-        console.log("onConnectionLost:"+responseObject.errorMessage);
-      }
-    }
-
-    function onMessageArrived(message) {
-      console.log("onMessageArrived:"+message.payloadString);
-      score += Number(message.payloadString);
-      document.getElementById("sub_mqtt").innerHTML = score;
-    }
-
-    function random(){
-      var min = 1;
-      var max = 16;
-      number = Math.floor(Math.random() * (+max - +min)) + +min;
-      document.getElementById("number").innerHTML = number;
-    }
+  function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+    score += Number(message.payloadString);
+    document.getElementById("sub_mqtt").innerHTML = "Score: "+score;
+  }
 </script>
