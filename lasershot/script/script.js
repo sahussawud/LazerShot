4e8c9a4cc91935bdae1dd8112d7aa06c3d29e2ab
Sukrit leelakornkij <42915021+toeiisk@users.noreply.github.com>
2019-08-20 04:02:19 +0700

    Add files via upload
    update file 20/8/2562

---
+++ lasershot/script/script.js
@@ -0,0 +1,8 @@
+/*$(document).ready(function() {
+  $('.colors div.color-swatch').on('click', function() {
+    
+    var colorcode = $(this).data('color');
+    colorcodelg = colorcode+86;
+       $('.leaderboard-box').css('background-image', 'linear-gradient(to bottom, white 40%, '+colorcode);
+  });
+});*/
